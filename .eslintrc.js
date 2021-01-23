daf9a3d93466fe3e6f43646aa8eb96790d8c38b8
lbavendanos <lbavendanos@gmail.com>
2021-01-22 22:46:08 -0500

    Fix webpack

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,12 +2,12 @@ module.exports = {
   env: {
     browser: true,
     es6: true,
-    node: true
+    node: true,
   },
   extends: ['eslint:recommended', 'plugin:prettier/recommended'],
   parserOptions: {
     ecmaVersion: 2018,
-    sourceType: 'module'
+    sourceType: 'module',
   },
-  rules: {}
+  rules: {},
 }
