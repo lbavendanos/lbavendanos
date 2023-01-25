010a010dc114c754850a7b9bf40ab8abe44cc217
lbavendanos <lbavendanos@gmail.com>
2023-01-24 21:39:31 -0500

    Update webpack mix

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,16 +1,5 @@
 const mix = require('laravel-mix')
 
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel applications. By default, we are compiling the CSS
- | file for the application as well as bundling up all the JS files.
- |
- */
-
 mix
   .js('src/js/app.js', 'dist/js/')
   .sass('src/sass/app.scss', 'css/')
@@ -26,3 +15,4 @@ mix
     files: ['dist/**/*.*'],
   })
   .setPublicPath('dist')
+  .disableNotifications()
